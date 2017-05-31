271c981d99ea5cf1011a1b40b45c2f809b420da5
Ricky <Ricky@vanricky>
2017-05-30 23:06:58 -0300

    Banco de dados

--- POO_06_Banco/src/java/com/domain/quiz/DataBase.java
+++ POO_06_Banco/src/java/com/domain/quiz/DataBase.java
@@ -1,11 +1,27 @@
 package com.domain.quiz;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sun.rmi.runtime.Log;
 
 public class DataBase {
-    public static final HashMap<String, Jogador> BD = new LinkedHashMap<>();
-    
-    
+
+    String login = "root";
+    String senha = "root";
+    public Connection con =  null;
+
+    public DataBase() {
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            String url = "jdbc:derby://localhost:1527/db_quiz";
+            con = DriverManager.getConnection(url, login, senha);
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
